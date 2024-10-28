a52199bbbd07216ac8276856242f1e4cfe60fe73
chhong <pnci1029@gmail.com>
2024-10-28 20:02:53 +0900

    main 작업중

--- FolioPark_FE/src/components/common/Main.tsx
+++ FolioPark_FE/src/components/common/Main.tsx
@@ -1,9 +1,33 @@
+import style from "../../style/common/main.module.scss"
+
 export function Main() {
 
 
     return (
         <>
+            <div className={style.mainWrapper}>
 
+                {/*header*/}
+                <div className={style.headerWrapper}>
+                    <div className={style.headerTitle}>
+                        <span className={style.text}>
+                            HOME PAGE TITLE
+                        </span>
+                    </div>
+                    <img className={`${style.homeButtons} ${style.editButton}`}/>
+                    <img className={`${style.homeButtons} ${style.homeButton}`}/>
+                </div>
+
+            {/*main portfolio*/}
+               <div className={style.bodyWithImageWrapper}>
+                   <div className={style.mainImageWrapper}>
+                        <img className={style.profileImage}/>
+                   </div>
+                   <div className={style.subtract}>
+                   </div>
+               </div>
+
+            </div>
         </>
     );
 }
