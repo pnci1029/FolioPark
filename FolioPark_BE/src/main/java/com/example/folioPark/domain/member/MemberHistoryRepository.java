685a5d866d634a60fa7851a7f81e7a557ee88530
chhong <pnci1029@gmail.com>
2024-11-03 13:24:32 +0900

    멤버 셋업

---
+++ FolioPark_BE/src/main/java/com/example/folioPark/domain/member/MemberHistoryRepository.java
@@ -0,0 +1,6 @@
+package com.example.folioPark.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberHistoryRepository extends JpaRepository<MemberHistory, Long> {
+}
